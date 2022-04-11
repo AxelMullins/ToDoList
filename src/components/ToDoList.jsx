b86f1228571c1003decd58a40237f7efb97a352a
Axel Mullins <axelmullins94@gmail.com>
2022-04-11 19:11:43 -0300

    Remove localStorage

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,61 +1,63 @@
-import React, { useState } from 'react'
-import { Container } from 'react-bootstrap';
-import AddItem from './AddItem';
-import Content from './Content';
-import SearchItem from './SearchItem';
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import AddItem from "./AddItem";
+import Content from "./Content";
+import SearchItem from "./SearchItem";
 
 const ToDoList = () => {
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
-    const [newItem, setNewItem] = useState('')
-    const [search, setSearch] = useState('')
-  
-    const setAndSaveItems = (newItems) => {
-      setItems(newItems);
-      localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-    }
-  
-    const addItem = (item) => {
-      const id = items.length ? items[items.length - 1].id + 1 : 1;
-      const myNewItem = { id, checked: false, item };
-      const listItems = [...items, myNewItem];
-      setAndSaveItems(listItems);
-    }
-  
-    const handleCheck = (id) => {
-      const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-      setAndSaveItems(listItems);
-    }
-  
-    const handleDelete = (id) => {
-      const listItems = items.filter((item) => item.id !== id);
-      setAndSaveItems(listItems);
-    }
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!newItem) return;
-      addItem(newItem);
-      setNewItem('');
-    }
-  
-    return (
-      <Container>
-        <AddItem
-          newItem={newItem}
-          setNewItem={setNewItem}
-          handleSubmit={handleSubmit}
-        />
-        <SearchItem
-          search={search}
-          setSearch={setSearch}
-        />
-        <Content
-          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />
-      </Container>
+  const [items, setItems] = useState([]);
+  // setItems(localStorage.getItem("shoppinglist"))
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
     );
-  }
+    setAndSaveItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+
+  return (
+    <Container>
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+    </Container>
+  );
+};
 
 export default ToDoList;
